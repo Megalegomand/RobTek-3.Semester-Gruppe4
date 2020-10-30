1bdc44576a564c3a368d07323f6148979ccecffb
Rasmus <rasmc19@student.sdu.dk>
2020-10-30 15:23:04 +0100

    Added Some Goertzel Functions

--- 3SemesterProjekt/3SemesterProjekt/DTMF.cpp
+++ 3SemesterProjekt/3SemesterProjekt/DTMF.cpp
@@ -28,8 +28,8 @@ sf::SoundBuffer buffer;
 }
 void DTMF::receiveDTMF()
 {
-    vector<sf::Int16> even;
-    vector<sf::Int16> odd;
+    vector<int> even;
+    vector<int> odd;
     using namespace std::this_thread; // sleep_for, sleep_until
     using namespace std::chrono; // nanoseconds, system_clock, seconds
     std::vector<std::string> availableDevices = sf::SoundRecorder::getAvailableDevices();
@@ -41,11 +41,37 @@ void DTMF::receiveDTMF()
     recorder.stop();
     const sf::SoundBuffer& buffer = recorder.getBuffer();
     buffer.saveToFile("my_record.ogg");
-    const sf::Int16* samples = buffer.getSamples();
+    const int* samples = buffer.getSamples();
     std::size_t count = buffer.getSampleCount();
-    /*doSomething(samples, count);*/
 
-    for (int i = 0; i < count; i+=2)
+    //Ovenstående optager lyd og gemmer det 
+    float coeff;
+    float Q1;
+    float Q2;
+    float sine;
+    float cosine;
+
+    float floatN;
+    float omega;
+
+    /*doSomething(samples, count);*/
+    floatN = (FLOATING)N;
+    k = (int)(0.5 + ((floatN *samples) / SAMPLING_RATE));
+    omega = (2.0 * PI * k) / floatN;
+    sine = sin(omega);
+    cosine = cos(omega);
+    coeff = 2.0 * cosine;
+
+    printf("For SAMPLING_RATE = %f", SAMPLING_RATE);
+    printf(" N = %d", N);
+    printf(" and FREQUENCY = %f,\n", samples);
+    printf("k = %d and coeff = %f\n\n", k, coeff);
+
+    ResetGoertzel();
+
+
+
+   /* for (int i = 0; i < count; i+=2)
     {
         even.push_back(samples[i]);
     }
@@ -54,12 +80,17 @@ void DTMF::receiveDTMF()
         odd.push_back(samples[i]);
     }
 
-   for (int k = 0; k < count / 2; k++)
+   for (int k = 0; k < count / 2; k++)*/
     {
         //Complex t = polar(1.0, -2 * PI * k / count) * odd[k];
         //samples[k] = even[k] + t;
         //samples[k + count / 2] = even[k] - t;
     }
+}
+void DTMF::ResetGoertzel()
+{
+    Q2 = 0;
+    Q1 = 0;
 }
 ;
 DTMF::~DTMF() {};
--- 3SemesterProjekt/3SemesterProjekt/DTMF.h
+++ 3SemesterProjekt/3SemesterProjekt/DTMF.h
@@ -17,6 +17,7 @@ public:
 	DTMF();
 	void playDTMF(int tonevalg);
 	void receiveDTMF();
+	void ResetGoertzel();
 	~DTMF();
 
 private:
@@ -25,6 +26,8 @@ private:
 	const double tonesL[4] = { 697.,770.,852.,941. };
 	const double tonesH[4] = { 1209.,1336.,1477.,1633. };
 	typedef complex<double> Complex;
+	int SAMPLING_RATE = 8000;
+	int N = 205; // forklaring til hvorfor vi bruger lige denne N sampling
 	
 };
 
