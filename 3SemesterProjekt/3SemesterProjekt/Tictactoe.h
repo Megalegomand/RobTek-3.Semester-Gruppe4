45ac8e3a7b71700027937844e13b7c18d117e25c
Simon C. Dockweiler <scdockweiler@gmail.com>
2020-12-15 18:52:31 +0100

    TicTacToe.h

--- 3SemesterProjekt/3SemesterProjekt/Tictactoe.h
+++ 3SemesterProjekt/3SemesterProjekt/Tictactoe.h
@@ -2,24 +2,23 @@
 #include<iostream>
 #include"DTMF.h"
 #include"FrameHandler.h"
+
 using namespace std;
-
-
+using namespace std::placeholders;
 class TicTacToe {
 public:
 	TicTacToe();
-	int game(); // runs the game
-	int checkwin(); // determines if the game have been won
-	void board(); // makes the board
-	void data(vector<char> data); // holder for data that needs to be send
-	void tokenpass(); // used to pass the turn
-	void end(); //used to End the session
+	int game(); // Run game
+	int checkwin(); // Determines if the game have been won
+	void board(); // Draw board on screen
+
+	void data(vector<char> data); // Data event from FrameHandler
+	void tokenpass(); // Token event from FrameHandler
+	void end(); // Closed event from FrameHandler
 
 
 private:
-	char square[10] = { 'o','1','2','3','4','5','6','7','8','9' }; //holds the pieces on the board 
-	int local = 0;// controls which player is local
-	int choicef = 0; // choice of foreign player
-
-
+	char square[10] = { 'o','1','2','3','4','5','6','7','8','9' }; // Pieces on the board
+	int local = 0; // Controls which player is local
+	int choicef = 0; // Choice of foreign player
 };
