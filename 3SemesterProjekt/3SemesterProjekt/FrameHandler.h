9f62f0b9301d8b9c5e38a9e1434693c57cf9c92f
Simon C. Dockweiler <scdockweiler@gmail.com>
2020-12-15 18:07:03 +0100

    FrameHandler.h

--- 3SemesterProjekt/3SemesterProjekt/FrameHandler.h
+++ 3SemesterProjekt/3SemesterProjekt/FrameHandler.h
@@ -7,9 +7,6 @@
 #include<mutex>
 #include <functional>
 
-// Temp
-#include"VirtuelDTMF.h"
-
 enum class TransmissionState {
 	NotConnected, 
 	Primary, // Has token, is able to send data along
@@ -22,75 +19,59 @@ using namespace concurrency;
 class FrameHandler
 {
 public:
-	const int BIND_WAIT_MIN = 10000;
-	const int BIND_WAIT_DIFF = 10000;
+	const int BIND_WAIT_MIN = 10000; // Minimum wait when binding
+	const int BIND_WAIT_DIFF = 10000; // Random time between BIND_WAIT_MIN and BIND_WAIT_MIN + BIND_WAIT_DIFF
 	const int ATTEMPTS = 10; // Attempts at sending before termination, does not account for bind
-	const int LISTEN_TIME = 5000;
+	const int LISTEN_TIME = 5000; // How long to listen for between each frame
 	const int MAX_LOSS_CONNECTION = ATTEMPTS * LISTEN_TIME * 2; // Maximum loss of connection, sync will be send at half this time
 
-
+	// For testing
 	int resend = 0;
 	int send = 0;
 
 
 	FrameHandler();
+	// Construtor with event functions, use std::bind for these
 	FrameHandler(function<void(vector<char>)> dataReadyEvent, function<void()> tokenPassEvent, function<void()> closed);
 
+	// Connect to another device
 	bool bind(int attempts);
 
-	bool sendData(vector<char> &data); // Returns true if succesfull
+	// Send a char array (byte array) as data
+	// Returns true if succesfull
+	bool sendData(vector<char>& data);
 
-	bool passToken(); // Pass token, return false if pass unsuccesful (Waiting or Conection failed)
+	// Pass token, return false if pass unsuccesful (Waiting or Conection failed)
+	bool passToken();
+
+	// Close connection
 	void close();
 
+	// Get current state
 	TransmissionState getState();
 private:
 	Frame* frame;
 	mutex frame_mutex;
 	TransmissionState state = TransmissionState::NotConnected;
-	thread* connected_thread;
+	thread* connected_thread; // Background thread running while connected
 
+	// Events for application layer
 	function<void(vector<char>)> dataReadyEvent;
 	function<void()> tokenPassEvent;
 	function<void()> closeEvent;
 
+	// Sequence numbers
 	bool dataSeqSend = 0;
 	bool dataSeqReceive = 0;
 
-	bool sendWaitACK(TransmissionType type, vector<char> &data); // Sends data and waits for ACK, tries ATTEMPTS times
-	bool sendWaitACK(TransmissionType type);					// Returns false if connection is lost
+	// Sends data and waits for ACK, tries ATTEMPTS times
+	// Returns false if all attempts have been used
+	bool sendWaitACK(TransmissionType type, vector<char>& data);
+	bool sendWaitACK(TransmissionType type);
+
+	// Function run in background thread
 	void connectedRun();
 
+	// Close connection without notifying other device
 	void terminate();
 };
-
-/*
-Preamble SFD (1tone)
-FA50   TYPE    DATA Checksum (No transmission)
-
-
-tone = 100ms
-
-
-1: Bind
-2: ACK
-
-//1: Connect
-//2: ACK
-1: Data
-2: ACK
-
-1: Token til 2
-2: ACK
-Time 100000 (
-	2: Connect
-	1: ACK
-)
-2: Connect
-1: ACK
-2: Data
-1: ACK
-2: Token til 1
-1: ACK
-
-*/
