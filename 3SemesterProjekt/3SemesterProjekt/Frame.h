1ccaf547f7eda8c9b7876547a3e16677792a8d3a
Simon C. Dockweiler <scdockweiler@gmail.com>
2020-12-15 17:22:05 +0100

    Frame.h

--- 3SemesterProjekt/3SemesterProjekt/Frame.h
+++ 3SemesterProjekt/3SemesterProjekt/Frame.h
@@ -4,6 +4,8 @@
 #include"VirtuelDTMF.h"
 #include"Timer.h"
 
+// All diferent types of transmission with tone value
+// NONE has a non existent tone
 enum TransmissionType {
 	NONE = 0xFF,
 	BIND = 0x0,
@@ -21,27 +23,32 @@ using namespace std;
 class Frame
 {
 public:
-	const char PREAMBLE[4] = { 0xF, 0xE, 0xD, 0xC };
+	const char PREAMBLE[4] = { 0xF, 0xE, 0xD, 0xC }; // Tones in preamble
 
 	Frame();
 
-	TransmissionType getType();
-	vector<char> getData();
+	TransmissionType getType(); // Get transmission type
+	vector<char> getData(); // Gets data as bytes, not tones
 
+	// Sends a frame witht type and data
 	void sendFrame(TransmissionType transmissionType); // Clears data
 	void sendFrame(TransmissionType transmissionType, vector<char> dataTones);
 
+	// Wait for a frame to arrive and update internal values with those from the frame
 	bool wait(int timeout); // Timeout in millis, returns successful
 
-	Timer* getLastActive(); // Returns milliseconds since last frame was recieved
+	// Timer object from when the last frame was send or received.
+	Timer* getLastActive();
 private:
-	
 	TransmissionType transmissionType = NONE;
-	vector<char> dataTones = vector<char>();
+	vector<char> dataTones = vector<char>(); // Contains tones, not bytes
+
 	DTMF* dtmf;
 	Timer* lastActive;
 
-	void send(); // Send data stored in frame
+	// Send data stored in frame
+	void send(); 
 	
+	// Calculate CRC16 checksum
 	unsigned short crc16(vector<char> data);
 };
