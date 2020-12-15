6f0116b3dfe7e771a14f4cd8c7dab537a0089dae
Simon C. Dockweiler <scdockweiler@gmail.com>
2020-12-15 19:02:05 +0100

    Timer.h

--- 3SemesterProjekt/3SemesterProjekt/Timer.h
+++ 3SemesterProjekt/3SemesterProjekt/Timer.h
@@ -10,9 +10,16 @@ class Timer
 public:
     Timer();
 
+    // Start og restart timer
     void start();
+
+    // Get milliseconds since start()
     int elapsedMillis();
+
+    // Get time of start()
     time_point<steady_clock> getStartTime();
+
+    // Sleep until offset millis has passed since start()
     void sleepUntil(int offset);
 private:
     steady_clock clock;
